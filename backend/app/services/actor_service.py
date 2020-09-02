95b650df7ae5344df849700cb0fa43eb701f46a3
ErickGiffoni <giffoni.erick@gmail.com>
2020-09-02 18:31:08 -0300

    get movie titles - function returns JSON error
    Co-authored-by: Elias Bernardo <ebmm01@gmail.com>

--- backend/app/services/actor_service.py
+++ backend/app/services/actor_service.py
@@ -8,9 +8,33 @@ class ActorService():
 
     def get_actor_data(self, actor_name: str) -> str:
         actor = self.imdb.search_person(actor_name)[0]
+        # <class 'imdb.Person.Person'>
 
-        full_person = self.imdb.get_person(actor.getID())
+        #full_person = self.imdb.get_person(actor.getID())
+        result = self.imdb.get_person(actor.getID())
+        ##############################
 
-        return full_person["filmography"]
+        filmography = self.imdb.get_person_filmography(actor.getID())
+        #<class 'dict'>
+
+        filmography = filmography["data"]["filmography"]
+        #<class 'list'>
+        filmography = filmography["data"]["filmography"][0]
+        #<class 'dict'>
+        chave = "actress"
+        if filmography.get("actor", False): # verifica se e um ator ou uma atriz e
+            chave = "actor"                 # muda a chave se for ator
+        
+        filmography = filmography[chave]
+        #<class 'list'> de Movie
+        filmes = [] # lista de filmes do ator/atriz
+        for i in filmography:
+            filmes.append(i["title"])
+        
+        #print("\n" + filmes + "\n")
+
+        #return full_person["filmography"][0]["actress"] #or full_person["filmography"][0]["actor"]
+
+        return result["filmography"]
 
 
