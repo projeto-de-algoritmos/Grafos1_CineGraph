1769ebdc9d271362ec331ccd27e2977b14507c6a
ErickGiffoni <giffoni.erick@gmail.com>
2020-09-02 19:31:09 -0300

    get movie data, cast data - graph almost done - still has to fix line 57
    Co-authored-by: Elias Bernardo <ebmm01@gmail.com>

--- backend/app/services/actor_service.py
+++ backend/app/services/actor_service.py
@@ -33,6 +33,32 @@ class ActorService():
         
         #print("\n" + filmes + "\n")
 
+        quantity = 5 # max 5 movies and max 5 people cast
+        countMovies = 0 # movie counter
+        countPeople = 0 # people counter
+
+        movies = [{}]
+
+        for i in filmes:
+            if countMovies==quantity:
+                break
+            filme = self.imdb.search_movie(i)
+            filme = filme[0]
+            movies[0]["movie_%d" % countMovies] = [{"data":""},{"cast":""}]
+            movies[0]["movie_%d" % countMovies][0]["data"] = filme.data
+            movies[0]["movie_%d" % countMovies][1]["cast"] = []
+            # above: movie data, such as title, kind, year, cover url
+            filme = self.imdb.get_movie(filme.movieID)
+            for j in filme.data["cast"]:
+                if countPeople==quantity:
+                    break
+                personID = j.personID
+                personData = self.imdb.get_person_main(personID)
+                movies[0]["movie_%d" % countMovies][1]["cast"].append(personData) # corrigir aqui
+                countPeople+=1
+            countPeople = 0
+            countMovies+=1
+
         #return full_person["filmography"][0]["actress"] #or full_person["filmography"][0]["actor"]
 
         return result["filmography"]
