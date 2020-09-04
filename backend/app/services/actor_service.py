6a78414cb864dab02694b1578dd40d2ef948f3ba
Elias Bernardo <ebmm01@gmail.com>
2020-09-03 21:35:25 -0300

    Adding graph structure

--- backend/app/services/actor_service.py
+++ backend/app/services/actor_service.py
@@ -6,6 +6,7 @@ class ActorService():
 
     def __init__(self):
         self.imdb = IMDb()
+        self.current_actor = ""
 
     def get_actors_by_name(self, actor_name: str) -> List:
         actorList = []
@@ -23,11 +24,12 @@ class ActorService():
 
         full_person = self.imdb.get_person(id, info=['main'])
         movieListByActor = self.__get_movieset_from_person(full_person)
-
+        self.current_actor = full_person.get('name')
         return [{
-            "person": full_person.get('name'),
+            "person": self.current_actor,
             "person_image": full_person.get('headshot'),
-            "movies": movieListByActor
+            "movies": movieListByActor,
+            "person_id": full_person.getID()
         }]
 
     def __get_movieset_from_person(self, person) -> List:
@@ -38,10 +40,26 @@ class ActorService():
         except Exception:
             full_person_movieset = next((item for item in person['filmography'] if item.get("actress")), None)['actress']
 
-        for movie in full_person_movieset:
+        for movie in full_person_movieset[0:15]:
+            localMovie = self.get_movie_data(movie.getID())
             movieListByActor.append({
-                "title":  movie['title'],
-                "id": movie.movieID
+                "title":  localMovie.get('title'),
+                "id": localMovie.movieID,
+                "cover": localMovie.get('cover'),
+                "cast": self.get_movie_cast(localMovie.get('cast')[0:5])
             })
 
         return movieListByActor
+    
+    def get_movie_data(self, movieID: str) -> List:
+        return self.imdb.get_movie(movieID)
+
+    def get_movie_cast(self, movie) -> List:
+        
+        def _mount_data_cast(person):
+            return {
+                "person": person.get('name'),
+                "person_id": person.getID()
+            }
+
+        return [ _mount_data_cast(person) for person in movie if person.get('name') is not self.current_actor ]
